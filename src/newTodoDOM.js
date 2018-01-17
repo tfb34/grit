a8ffe6fe31cb4855458e95d734b550046e13c4b6
tfb34 <britothalia212@gmail.com>
2018-01-17 12:33:30 -0500

    Fix referenceError

--- src/newTodoDOM.js
+++ src/newTodoDOM.js
@@ -30,9 +30,8 @@ function createNewTodo(){
 	let priority = sel.options[sel.selectedIndex].text;
 	// for now this date value will due
 	let today = new Date();
-	let d = today.toDateString();
-    console.log(d);
-	let todo = new Todo(task, d, priority);
+    console.log(today.toDateString());
+	let todo = new Todo(task, today.toDateString(), priority);
     console.log(todo);
 	//add new element to project
     let project = projects.get(document.getElementById("project").innerHTML);
